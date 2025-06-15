d6dd826b5a940e340547520cc790693e8009445b
Ilya Kobtsev <kobtsevmail100@mail.ru>
2025-06-15 18:26:29 +0300

    add model

--- orders/backend/models.py
+++ orders/backend/models.py
@@ -22,6 +22,7 @@ USER_TYPE_CHOICES = (
 
 
 class Profile(AbstractUser):
+    """Модель пользователя"""
     username_validator = UnicodeUsernameValidator()
     username = models.CharField(
         _('username'),
@@ -48,6 +49,7 @@ class Profile(AbstractUser):
 
 
 class Shop(models.Model):
+    """Модель магазина"""
     class Shop(models.Model):
         title = models.CharField(max_length=250, verbose_name='Название')
         address = models.CharField(max_length=100)
@@ -68,6 +70,7 @@ class Shop(models.Model):
 
 
 class Category(models.Model):
+    """Модель категорий"""
     name = models.CharField(verbose_name='Название', max_length=100)
     shops = models.ManyToManyField(Shop, verbose_name='Магазины', related_name='categories', blank=True)
 
@@ -81,6 +84,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    """Модель продукта"""
     name = models.CharField(max_length=80, verbose_name='Название')
     category = models.ForeignKey(Category, verbose_name='Категория', related_name='products', blank=True,
                                  on_delete=models.CASCADE)
@@ -92,3 +96,161 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductInfo(models.Model):
+    """Модель информации о продукте"""
+    model = models.CharField(max_length=80, verbose_name='Модель', blank=True)
+    external_id = models.PositiveIntegerField(verbose_name='Внешний ИД')
+    product = models.ForeignKey(Product,verbose_name="Продукт",
+        related_name="product_info", blank=True, on_delete=models.CASCADE
+    )
+    shop = models.ForeignKey(Shop,verbose_name="Магазин",
+        related_name="product_info", blank=True, on_delete=models.CASCADE
+    )
+    quantity = models.IntegerField("Количество в наличии")
+    price = models.IntegerField("Цена")
+    price_rrc = models.IntegerField("Рекомендованная цена")
+
+    class Meta:
+        verbose_name = "Информация о продукте"
+        verbose_name_plural = "Информационный список о продуктах"
+        constraints = [
+            models.UniqueConstraint(fields=['product', 'shop', 'external_id'], name='unique_product_info'),
+        ]
+
+    def __str__(self):
+        return f"{self.product.name} {self.shop.name}"
+
+
+class Parameter(models.Model):
+    """Модель параметров"""
+    name = models.CharField(max_length=40, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Имя параметра'
+        verbose_name_plural = "Список имен параметров"
+        ordering = ('-name',)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductParameter(models.Model):
+    """Модель параметров продукта"""
+    product_info = models.ForeignKey(ProductInfo, verbose_name='Информация о продукте',
+                                     related_name='product_parameters', blank=True,
+                                     on_delete=models.CASCADE)
+    parameter = models.ForeignKey(Parameter, verbose_name='Параметр', related_name='product_parameters', blank=True,
+                                  on_delete=models.CASCADE)
+    value = models.CharField(verbose_name='Значение', max_length=100)
+
+    class Meta:
+        verbose_name = 'Параметр'
+        verbose_name_plural = "Список параметров"
+        constraints = [
+            models.UniqueConstraint(fields=['product_info', 'parameter'], name='unique_product_parameter'),
+        ]
+
+    def __str__(self):
+        return f'{self.parameter.name}: {self.value}'
+
+
+class Contact(models.Model):
+    """Модель контактов пользователя"""
+    user = models.ForeignKey(Profile, verbose_name='Пользователь',
+                             related_name='contacts', blank=True,
+                             on_delete=models.CASCADE)
+
+    city = models.CharField(max_length=50, verbose_name='Город')
+    street = models.CharField(max_length=100, verbose_name='Улица')
+    house = models.CharField(max_length=15, verbose_name='Дом', blank=True)
+    structure = models.CharField(max_length=15, verbose_name='Корпус', blank=True)
+    building = models.CharField(max_length=15, verbose_name='Строение', blank=True)
+    apartment = models.CharField(max_length=15, verbose_name='Квартира', blank=True)
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+
+    class Meta:
+        verbose_name = 'Контакты пользователя'
+        verbose_name_plural = "Список контактов пользователя"
+
+    def __str__(self):
+        return f'{self.city} {self.street} {self.house}'
+
+
+class Order(models.Model):
+    """Модель заказов"""
+    user = models.ForeignKey(Profile, verbose_name='Пользователь',
+                             related_name='orders', blank=True,
+                             on_delete=models.CASCADE)
+    dt = models.DateTimeField(auto_now_add=True)
+    state = models.CharField(verbose_name='Статус', choices=ORDER_STATE_CHOICES, max_length=15)
+    contact = models.ForeignKey(Contact, verbose_name='Контакт',
+                                blank=True, null=True,
+                                on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = "Список заказ"
+        ordering = ('-dt',)
+
+    def __str__(self):
+        return str(self.dt)
+
+
+class OrderItem(models.Model):
+    """Модель позиции заказа"""
+    order = models.ForeignKey(Order, verbose_name='Заказ', related_name='ordered_items', blank=True,
+                              on_delete=models.CASCADE)
+
+    product_info = models.ForeignKey(ProductInfo, verbose_name='Информация о продукте', related_name='ordered_items',
+                                     blank=True,
+                                     on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name='Количество')
+
+    class Meta:
+        verbose_name = 'Заказанная позиция'
+        verbose_name_plural = "Список заказанных позиций"
+        constraints = [
+            models.UniqueConstraint(fields=['order_id', 'product_info'], name='unique_order_item'),
+        ]
+
+
+class ConfirmEmailToken(models.Model):
+    """модель подтверждения электронной почты"""
+    class Meta:
+        verbose_name = 'Токен подтверждения Email'
+        verbose_name_plural = 'Токены подтверждения Email'
+
+    @staticmethod
+    def generate_key():
+        """ generates a pseudo random code using os.urandom and binascii.hexlify """
+        return get_token_generator().generate_token()
+
+    user = models.ForeignKey(
+        Profile,
+        related_name='confirm_email_tokens',
+        on_delete=models.CASCADE,
+        verbose_name=_("The User which is associated to this password reset token")
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("When was this token generated")
+    )
+
+    # Key field, though it is not the primary key of the model
+    key = models.CharField(
+        _("Key"),
+        max_length=64,
+        db_index=True,
+        unique=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super(ConfirmEmailToken, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return "Password reset token for user {user}".format(user=self.user)
