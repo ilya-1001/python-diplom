dec7f066e53443b083e560e4d782841fd9248621
Ilia Kobtsev <ikobtsev83@yandex.ru>
2025-06-16 22:34:37 +0300

    add one serializers

---
+++ orders/backend/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Profile, Category, Shop, ProductInfo, Product, ProductParameter, OrderItem, Order, Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ('id', 'city', 'street', 'house', 'structure', 'building', 'apartment', 'user', 'phone')
+        read_only_fields = ('id',)
+        extra_kwargs = {
+            'user': {'write_only': True}
+        }
